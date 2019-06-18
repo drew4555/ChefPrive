64bf0e509a39f5d7830168dfb4ce7456a9381b21
andrew carr <drew4555@yahoo.com>
2019-06-18 11:23:25 -0500

    created Initial Models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ChefPrive.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,15 @@ namespace ChefPrive.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<WeeklyMealPlan> WeeklyMealPlans { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
     }
 }
--- Models/Client.cs
+++ Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace ChefPrive.Models
 {
     public class Client
     {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int ZipCode { get; set; }
+        public string FirstName { get; set; }
+        public bool Vegetarian { get; set; }
+        public bool Vegan { get; set; }
+        
+       
     }
 }
