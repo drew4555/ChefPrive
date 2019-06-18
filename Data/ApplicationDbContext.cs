82cde5bfff7749f0e44e50cd4cf994dfa3ee9d88
andrew carr <drew4555@yahoo.com>
2019-06-18 13:12:49 -0500

    created tables with migration1

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace ChefPrive.Data
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<WeeklyMealPlan> WeeklyMealPlans { get; set; }
+        public DbSet<ClientIngredient> ClientIngredients { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
