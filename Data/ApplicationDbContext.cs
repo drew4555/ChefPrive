78336e452379e9aebfad66505cd459a12bf1277a
unknown <soccer_Matt_4@yahoo.com>
2019-06-19 11:42:41 -0500

    Implemented code to make button clicks link to the different funtions of the website.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,7 +14,6 @@ namespace ChefPrive.Data
         {
         }
         public DbSet<Client> Clients { get; set; }
-        public DbSet<Favorite> Favorites { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<WeeklyMealPlan> WeeklyMealPlans { get; set; }
