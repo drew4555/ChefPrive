928502b8537a92cfb85578781acc5b80de929e4d
unknown <mr.danieljberg@gmail.com>
2019-06-21 13:07:31 -0500

    pulling to make web app up to date

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -4,10 +4,6 @@ using System.Text;
 
 namespace Application
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 1a6a71616bb9571d6d3ce249ca60e87355dc2579
     public class Class1
 
     {
--- Infrastructure/IRecipeClient.cs
+++ Infrastructure/IRecipeClient.cs
@@ -8,5 +8,9 @@ namespace Infrastructure
     public interface IRecipeClient
     {
         JObject GetRecipeById(string id);
+        JArray SearchRecipeByIngredients(int numberOfRecipesReturned, string ingredients);
+        JObject SearchRecipe(string diet, string excludeIngredients, string intolerances, int numberOfRecipes, string type, string query);
+        JArray RecommendRecipes(string id);
+        JObject GetMealPlan(string timeFrame, int targetCalories, string diet, string exclude);
     }
 }
