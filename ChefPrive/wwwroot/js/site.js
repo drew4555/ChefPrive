76ddc6bf3c547ba7703c5b698ce59fc371c1c11a
unknown <soccer_Matt_4@yahoo.com>
2019-06-23 17:27:06 -0500

    Adding js functions to project to try and query the api to search for recipes.

--- ChefPrive/wwwroot/js/site.js
+++ ChefPrive/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var api = '';
+var apiKey = '';
+var input;
+
+
+function setup() {
+
+    var ;
+
+
+    input = select('flexsearch--submit');
+}
+
+
+function SearchRecipes() {
+    var url = api + input.value() + apiKey + ingredients;
+    loadJson(url, gotData);
+}
+
+function gotData(data) {
+    recipes = data;
+}
